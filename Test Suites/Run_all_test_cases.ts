1cb2056edaf8a22638706f2df2fff9be46dcbaf7
Nguyen Xuan Tam <xuantam0304@gmail.com>
2020-06-06 21:00:53 +0700

    Rename test suite to run_all_test_cases as for unable to open error

--- Test Suites/Run_all_test_cases.ts
+++ Test Suites/Run_all_test_cases.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>New Test Suite</name>
+   <name>Run_all_test_cases</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
